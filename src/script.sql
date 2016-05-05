617225675936bcc7f11c39aca470c02087dcff03
Max Donchenko <wereya2@gmail.com>
2016-05-06 01:56:25 +0300

    Update SQL script for DB

--- src/script.sql
+++ src/script.sql
@@ -9,6 +9,8 @@ USE solving_tasks;
 DROP TABLE IF EXISTS users;
 CREATE TABLE IF NOT EXISTS users (
   id INT NOT NULL UNIQUE AUTO_INCREMENT,
+  login VARCHAR(45) NULL UNIQUE,
+  password VARCHAR(45) NULL,
   name VARCHAR(45) NULL,
   surname VARCHAR(45) NULL,
   type INT NOT NULL,
@@ -24,9 +26,14 @@ DROP TABLE IF EXISTS orders;
 CREATE TABLE IF NOT EXISTS orders (
   id INT NOT NULL AUTO_INCREMENT,
   id_customer INT NOT NULL,
-  id_executor INT NOT NULL,
-  datetime_start DATETIME,
-  datetime_end DATETIME,
+  id_executor INT,
+  task TEXT,
+  datetime_start DATE,
+  datetime_end DATE,
   PRIMARY KEY (id)
 )
   ENGINE = InnoDB;
+
+INSERT INTO users (login, password, name, surname, type) VALUES ('maximdonchenko', 'maxim123', 'Max', 'Donchenko', 3);
+INSERT INTO users (login, password, name, surname, type) VALUES ('pavelshevchenko', 'pavel123', 'Pavel', 'Shevchenko', 1);
+INSERT INTO users (login, password, name, surname, type) VALUES ('test', 'test', 'TestName', 'TestSurname', 2);
