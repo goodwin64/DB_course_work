99195e8b330d8fd081ad312f25de7c248c9bf0db
Max Donchenko <wereya2@gmail.com>
2016-05-06 02:55:17 +0300

    New features (correct authorization, check the new task, calendar panel in the "new task" form, different message/option pop up panels)

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,9 @@
  * Created by Max Donchenko (https://github.com/goodwin64) on 27.04.2016.
  */
 public class Main {
+    static int loggedUser;
+
     public static void main(String[] args) {
         Authorization authorization = new Authorization();
-        NewTask newTask = new NewTask();
     }
 }
